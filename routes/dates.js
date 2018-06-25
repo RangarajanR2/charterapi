b27f69e51289e1bc0bbbb7bca8def7c8912bb936
R2 <r2@bajajfinserv.in>
2018-06-25 08:00:50 +0530

    Travel date

--- routes/dates.js
+++ routes/dates.js
@@ -8,4 +8,9 @@ router.get('/', function (req, res, next) {
     res.render('date',{param:req.query});
 });
 
+router.get('/bot', function (req, res, next) {
+    // console.log({param:req});
+    res.render('relations');
+});
+
 module.exports = router;
