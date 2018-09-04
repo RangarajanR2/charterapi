56d61d0f2ad8abaf54cb6bda9e705b4ec72f38ff
RangarajanR2 <arrangarajan.karthik@gmail.com>
2018-09-04 17:51:06 +0530

    dummy

--- index.js
+++ index.js
@@ -67,14 +67,16 @@ let sendEmailBeta = function(email){
 };
  
 let prevHourCalc = function(){
-    let time = new Date(moment().tz("Asia/Calcutta").format());
+    let time = new Date();
+    time = new Date(time + time.getTimezoneOffset());
     
     return new Date(time.getFullYear(), time.getMonth(), time.getDate() , time.getHours()-1, time.getMinutes(), time.getSeconds(), time.getMilliseconds())
 }
 
 let nextHourCalc = function(){
     console.log(new Date());
-    let time = new Date(moment().tz("Asia/Calcutta").format());
+    let time = new Date();
+    time = new Date(time + time.getTimezoneOffset());
     return new Date(time.getFullYear(), time.getMonth(), time.getDate() , time.getHours()+1, 0, 0, 0) - time;
 }
 
@@ -82,79 +84,79 @@ let nextHour = nextHourCalc();
 
 console.log("next hour : "+nextHour);
 
-setTimeout(function(){
+// setTimeout(function(){
 
-    let action = function(){
-        let currentHour = new Date();
-        // let x-auth-token
-        let prevHour = prevHourCalc();
-        console.log(prevHour);
-        let html = "Hi, <br><br>";
-        let tables = ["policy_wordings", "policy_brochures"];
+//     let action = function(){
+//         let currentHour = new Date();
+//         // let x-auth-token
+//         let prevHour = prevHourCalc();
+//         console.log(prevHour);
+//         let html = "Hi, <br><br>";
+//         let tables = ["policy_wordings", "policy_brochures"];
         
-        let promises = []
-        for(let tableKey in tables){
-            let table = tables[tableKey];
-            let options = {
-                url : 'https://botplatform.io/admin/data/records/'+table+'/0-500?bot=bot_1498199350938',
-                headers : {
-                    'x-auth-token' : '539d3f82c4dda3b52a21ef10ea3ab58c2e7173fc5cd3ae4d360c19eb643afbdd'
-                },
-                method : 'GET'
-            }
-            console.log(options)
-            promises.push(axios(options).then( (results) => {
-                // console.log(results.data.data);
-                let resultset = [];
-                results.data.data.forEach(function(data) {
-                    if(new Date(data.insertedDate) > prevHour){
-                        resultset.push(data);
-                    }
-                })
-                console.log(resultset.length);
-                if(resultset.length > 0){
-                    html += "<br>New entries to the "+table+" in the last hour<br><br>";
-                    let tableString = "<table border='1'>";
-                    delete resultset[0].insertedDate;
-                    delete resultset[0].updatedDate;
-                    delete resultset[0]._id;
-                    let keys = Object.keys(resultset[0]);
-                    keys.forEach(function(headerKey){
-                        tableString += " <th>"+headerKey+"</th>"
-                    })
-                    resultset.forEach(function(data){
-                        let htmlString = "<tr>";
-                        delete data.insertedDate;
-                        delete data.updatedDate;
-                        delete data._id;
-                        Object.keys(data).forEach(function(key){
-                            htmlString += "<td>"+data[key]+"</td>"
-                        })
-                        htmlString += "</tr>"
-                        tableString += htmlString;
-                    })
-                    tableString += "</table>"
-                    html += tableString;
-                }else{
-                    html += "<br>There has been no new addition to the "+table+" in the last hour <br><br>";
-                }
-            }))
-        }
-        Promise.all(promises).then( () => {
-            console.log(html);
-            sendEmailBeta({
-                to : to,
-                cc : cc,
-                html : html,
-                subject : 'Hourly updates',
-            })
+//         let promises = []
+//         for(let tableKey in tables){
+//             let table = tables[tableKey];
+//             let options = {
+//                 url : 'https://botplatform.io/admin/data/records/'+table+'/0-500?bot=bot_1498199350938',
+//                 headers : {
+//                     'x-auth-token' : '539d3f82c4dda3b52a21ef10ea3ab58c2e7173fc5cd3ae4d360c19eb643afbdd'
+//                 },
+//                 method : 'GET'
+//             }
+//             console.log(options)
+//             promises.push(axios(options).then( (results) => {
+//                 // console.log(results.data.data);
+//                 let resultset = [];
+//                 results.data.data.forEach(function(data) {
+//                     if(new Date(data.insertedDate) > prevHour){
+//                         resultset.push(data);
+//                     }
+//                 })
+//                 console.log(resultset.length);
+//                 if(resultset.length > 0){
+//                     html += "<br>New entries to the "+table+" in the last hour<br><br>";
+//                     let tableString = "<table border='1'>";
+//                     delete resultset[0].insertedDate;
+//                     delete resultset[0].updatedDate;
+//                     delete resultset[0]._id;
+//                     let keys = Object.keys(resultset[0]);
+//                     keys.forEach(function(headerKey){
+//                         tableString += " <th>"+headerKey+"</th>"
+//                     })
+//                     resultset.forEach(function(data){
+//                         let htmlString = "<tr>";
+//                         delete data.insertedDate;
+//                         delete data.updatedDate;
+//                         delete data._id;
+//                         Object.keys(data).forEach(function(key){
+//                             htmlString += "<td>"+data[key]+"</td>"
+//                         })
+//                         htmlString += "</tr>"
+//                         tableString += htmlString;
+//                     })
+//                     tableString += "</table>"
+//                     html += tableString;
+//                 }else{
+//                     html += "<br>There has been no new addition to the "+table+" in the last hour <br><br>";
+//                 }
+//             }))
+//         }
+//         Promise.all(promises).then( () => {
+//             console.log(html);
+//             sendEmailBeta({
+//                 to : to,
+//                 cc : cc,
+//                 html : html,
+//                 subject : 'Hourly updates',
+//             })
 
-        })
+//         })
         
 
-    };
-    action()
-    setInterval(action, 3600000)
+//     };
+//     action()
+//     setInterval(action, 3600000)
 
-}, nextHour)
+// }, nextHour)
 }
